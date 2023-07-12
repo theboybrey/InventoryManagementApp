a619609f7738ee94393eb1b221535358044d6fa1
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-07-12 16:14:15 -0400

    Repo updated with Stock & Drug Supplier

---
+++ src/main/java/com/example/cosignshop/Repository/DrugSupplier.java
@@ -0,0 +1,17 @@
+package com.example.cosignshop.Repository;
+
+public class DrugSupplier {
+    private int drugId;
+    private int supplierId;
+    // Other attributes, constructors, getters, setters, and methods
+
+    // Update the constructor to include all relevant fields
+    public DrugSupplier(int drugId, int supplierId) {
+        this.drugId = drugId;
+        this.supplierId = supplierId;
+    }
+
+    // Update the getters and setters for all fields
+    // ...
+
+}
---
+++ src/main/java/com/example/cosignshop/Repository/Stock.java
@@ -0,0 +1,17 @@
+package com.example.cosignshop.Repository;
+
+public class Stock {
+    private int drugId;
+    private int quantity;
+    // Other attributes, constructors, getters, setters, and methods
+
+    // Update the constructor to include all relevant fields
+    public Stock(int drugId, int quantity) {
+        this.drugId = drugId;
+        this.quantity = quantity;
+    }
+
+    // Update the getters and setters for all fields
+    // ...
+
+}
